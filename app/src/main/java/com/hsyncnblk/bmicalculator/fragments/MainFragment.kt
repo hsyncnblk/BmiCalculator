7c5b61c615585433dd7f3e6cd5d3177b9713ef1b
Hüseyin Çinibulak <huseyincnblk@gmail.com>
2023-09-07 21:33:32 +0300

    Main fragment completed.

--- app/src/main/java/com/hsyncnblk/bmicalculator/fragments/MainFragment.kt
+++ app/src/main/java/com/hsyncnblk/bmicalculator/fragments/MainFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import com.hsyncnblk.bmicalculator.R
 import com.hsyncnblk.bmicalculator.databinding.FragmentMainBinding
+import kotlin.math.pow
 
 
 class MainFragment : Fragment() {
@@ -19,7 +20,40 @@ class MainFragment : Fragment() {
     ): View? {
         binding=FragmentMainBinding.inflate(layoutInflater)
         return binding.root
+
+        binding.calculateBtn.setOnClickListener {
+            calculate()
+        }
+
     }
+
+    private fun calculate(){
+        val weight = binding.weightEdit.text.toString().toFloatOrNull()
+        val height = binding.heightEdit.text.toString().toFloatOrNull()
+
+        if (weight !=null && height !=null){
+
+            val bmi = weight/(height/100).pow(2)
+            val bmiResult = String.format("%.2f",bmi)
+
+            val bmiCategory=when{
+                bmi<18.5 -> "Underweight"
+                bmi<25 -> "Normal weight"
+                bmi<30 -> "Overweight"
+                else -> "Obese"
+            }
+
+            binding.resultText.text="BMI: $bmiResult \nCatagory: $bmiCategory"
+
+
+
+        }else{
+            binding.resultText.text="Invalid input"
+        }
+
+
+    }
+
 
 
 }
